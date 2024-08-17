6c3b94766a2b1af8e2c0bd095200d142b04f3fbb
atlantis-11 <85559317+atlantis-11@users.noreply.github.com>
2024-08-17 12:36:00 +0300

    Initial commit

---
+++ app/src/main/java/com/yevhenii/audiobooksyncer/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.yevhenii.audiobooksyncer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yevhenii.audiobooksyncer.ui.theme.AudiobookSyncerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AudiobookSyncerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AudiobookSyncerTheme {
+        Greeting("Android")
+    }
+}
